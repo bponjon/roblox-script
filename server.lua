2d3826aa19adf0c7b758c0e83c15b7163af75b0f
bponjon <wobon63@gmail.com>
2025-10-27 15:34:10 +0700

    Add server.lua to execute shell commands

---
+++ server.lua
@@ -0,0 +1,14 @@
+const { exec } = require('child_process');
+
+const command = process.argv[2];
+
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`);
+  }
+  console.log(`Stdout: ${stdout}`);
+});
